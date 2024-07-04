17454ddbec79163de955a9cdae2d4a1f33b09cdb
LeahJKH <leahjb06.kodehode@gmail.com>
2024-07-04 19:27:56 +0200

    object with info

---
+++ js/SmallLogic/projectsFiles.js
@@ -0,0 +1,34 @@
+export let projectsFiles = {
+    1: {
+        name: "Per Clicker",
+        link: ""
+    },
+    2: {
+        name: "MikuVite",
+        link: "mikuvite.netlify.app"
+    },
+    3: {
+        name: "Meows",
+        link: ""
+    },
+    4: {
+        name: "CodeCatalyst",
+        link: "https://leahjkh.github.io/typingapp/"
+    },
+    5: {
+        name: "Miku Blog",
+        link: "keen-churros-447906.netlify.app/"
+    },
+    6: {
+        name: "",
+        link: ""
+    },
+    7: {
+        name: "",
+        link: ""
+    },
+    8: {
+        name: "",
+        link: ""
+    }
+}
