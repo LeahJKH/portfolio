6842d2a9ebede857e7c4e5d65f99bbb8b4900e30
LeahJKH <leahjb06.kodehode@gmail.com>
2024-08-15 15:34:43 +0200

    cleaning

--- js/apps/binary.js
+++ js/apps/binary.js
@@ -1,93 +1,53 @@
+import { topCreator } from "./templates/TopMenu.js";
+
 export function setupBinary() {
-  const element = document.createElement("div");
-  element.classList.add("app");
-  const img = document.createElement("img");
-  const p = document.createElement("p");
-  const txt = document.createTextNode("Binary.txt");
-  p.appendChild(txt);
-  img.src = "./appIcons/notepad.png";
-  img.setAttribute("binary-file", "ID");
-  element.appendChild(img);
-  element.appendChild(p);
-
-  const page = document.createElement("div");
-  page.id = "binary-full";
-  page.className = "resizable";
-  page.style.display = "none";
-
-
-
-  const columnDiv = document.createElement('div');
-  columnDiv.className = 'column full-width';
-  
-  const topMenuDiv = document.createElement('div');
-  topMenuDiv.className = 'top-menu draggable full-width';
-  
-  const topRowDiv = document.createElement('div');
-  topRowDiv.className = 'row';
-  
-  const topImg = document.createElement('img');
-  topImg.src = './appIcons/notepad.png';
-  topImg.alt = '';
-  topImg.className = 'top-menu-pic';
-  topRowDiv.appendChild(topImg);
-  
-  const topH4 = document.createElement('h4');
-  topH4.textContent = 'binary.txt';
-  topRowDiv.appendChild(topH4);
-  
-  topMenuDiv.appendChild(topRowDiv);
-  
-  const buttonContainerDiv = document.createElement('div');
-  const minimizeButton = document.createElement('button');
-  minimizeButton.className = 'top-btns-style';
-  minimizeButton.textContent = '-';
-
-  const maximizeButton = document.createElement('button');
-  maximizeButton.className = 'top-btns-style';
-  maximizeButton.textContent = '[]';
-
-  const close = document.createElement("button");
-
-  close.className = 'closeX';
-  close.textContent = "X";
-
-  buttonContainerDiv.appendChild(minimizeButton);
-  buttonContainerDiv.appendChild(maximizeButton);
-  buttonContainerDiv.appendChild(close);
-  topMenuDiv.appendChild(buttonContainerDiv);
-  columnDiv.appendChild(topMenuDiv);
-  
+    
+    const { columnDiv, closeButton } = topCreator("binary.txt", "./appIcons/notepad.png", "Binary");
 
   
-  const secondRowDiv = document.createElement('div');
-  secondRowDiv.className = 'row';
-  
-  const binaryPara = document.createElement('p');
-  binaryPara.id = 'binary-code';
-  binaryPara.innerHTML = `
-    01001000 01100101 01111001 00100000 01111001 01101111 01110101 00100000 01100110
-    01101111 01110101 01101110 01100100 00100000 01110100 01101000 01100101 00100000
-    01100010 01101001 01101110 01100001 01110010 01111001 00100000 01100011 01101111
-    01100100 01100101 00101110 00100000 01110100 01101000 01100101 00100000 01110000
-    01100001 01110011 01110011 01110111 01101111 01110010 01100100 00100000 01101001
-    01110011 00100000 00111000 00110101 00110111 00110010 00100000 01100111 01101111
-    01101111 01100100 00100000 01101100 01110101 01100011 01101011 00100001
-  `;
-  
-  secondRowDiv.appendChild(binaryPara);
-  
-  columnDiv.appendChild(secondRowDiv);
-  
-  const resizerDiv = document.createElement('div');
-  resizerDiv.className = 'resizer';
-  columnDiv.appendChild(resizerDiv);
-  
-  page.appendChild(columnDiv);
+    const secondRowDiv = document.createElement('div');
+    secondRowDiv.className = 'row';
 
-  document.body.appendChild(page);
+    const binaryPara = document.createElement('p');
+    binaryPara.id = 'binary-code';
+    binaryPara.innerHTML = `
+        01001000 01100101 01111001 00100000 01111001 01101111 01110101 00100000 01100110
+        01101111 01110101 01101110 01100100 00100000 01110100 01101000 01100101 00100000
+        01100010 01101001 01101110 01100001 01110010 01111001 00100000 01100011 01101111
+        01100100 01100101 00101110 00100000 01110100 01101000 01100101 00100000 01110000
+        01100001 01110011 01110011 01110111 01101111 01110010 01100100 00100000 01101001
+        01110011 00100000 00111000 00110101 00110111 00110010 00100000 01100111 01101111
+        01101111 01101100 01110101 01100011 01101011 00100001
+    `;
 
-  return { element, page, close };
+    secondRowDiv.appendChild(binaryPara);
+   
+    columnDiv.appendChild(secondRowDiv);
+
+   
+    const resizerDiv = document.createElement('div');
+    resizerDiv.className = 'resizer';
+    columnDiv.appendChild(resizerDiv);
+
+    const page = document.createElement('div');
+    page.id = 'binary-full';  
+    page.appendChild(columnDiv);
+    
+  
+    const element = document.createElement("div");
+    element.classList.add("app");
+
+    const img = document.createElement("img");
+    const p = document.createElement("p");
+    const txt = document.createTextNode(`binary.txt`);
+
+    p.appendChild(txt);
+    img.src = "./appIcons/notepad.png";
+    img.setAttribute("binary-file", "ID");
+    element.appendChild(img);
+    element.appendChild(p);
+    
+    return { element, page, close: closeButton };
 }
 
 export function binaryClicked() {
